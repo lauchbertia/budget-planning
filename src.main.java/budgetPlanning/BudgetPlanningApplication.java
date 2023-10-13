0835db4285892ab2fdd6d4297b3adde5a6fb8ce8
lauchbertia <111848605+lauchbertia@users.noreply.github.com>
2023-10-13 14:31:48 +0200

    update gson

--- src.main.java/budgetPlanning/BudgetPlanningApplication.java
+++ src.main.java/budgetPlanning/BudgetPlanningApplication.java
@@ -1,9 +1,19 @@
 package budgetPlanning;
+import com.google.gson.Gson;
 
 public class BudgetPlanningApplication {
 
 	public static void main(String[] args) {
 		System.out.print("jf");
+		
+		Gson gson = new Gson();
+		String jsonString = "{\"name\":\"John\",\"age\":30}";
+		//System.out.print(gson);
+		pERSON myObject = gson.fromJson(jsonString, pERSON.class);
+		
+		System.out.println(myObject.getName());
 	}
+	
 
 }
+
