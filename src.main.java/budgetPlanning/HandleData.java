37a9e6e4dc2e110a14387c74731fd936b4391fca
lareidan <nina.lareida@roche.com>
2023-10-19 22:15:10 +0200

    get sum of balance of month method

--- src.main.java/budgetPlanning/HandleData.java
+++ src.main.java/budgetPlanning/HandleData.java
@@ -68,88 +68,159 @@ public class HandleData {
 		}
 		return dataList;
 	}
+	
+	public void getSumOfBalanceOfMonth() {
+	    Data data = new Data();
+	    DataContainer myObject = data.gson.fromJson(data.jsonString, DataContainer.class);
+	    List<HandleData> dataList = myObject.getData();
 
-	public List getSumOfBalanceOfMonth() {
+	    String currentMonth = "";
+	    double totalIncome = 0;
+	    double totalExpenses = 0;
+
+	    for (HandleData item : dataList) {
+	        String month = item.getMonth();
+	        double value = item.getValue();
+	        boolean isExpense = item.getExpenses();
+
+	        if (!month.equals(currentMonth)) {
+	            if (!currentMonth.isEmpty()) {
+	                // Print the totals for the previous month
+	                System.out.println("Month: " + currentMonth);
+	                System.out.println("Income: " + totalIncome);
+	                System.out.println("Expenses: " + totalExpenses);
+	            }
+	            
+	            // Reset totals for the new month
+	            currentMonth = month;
+	            totalIncome = 0;
+	            totalExpenses = 0;
+	        }
+
+	        if (isExpense) {
+	            totalExpenses += value;
+	        } else {
+	            totalIncome += value;
+	        }
+	    }
+
+	    // Print the totals for the last month
+	    System.out.println("");
+	    System.out.println("Month: " + currentMonth);
+	    System.out.println("Income: " + totalIncome);
+	    System.out.println("Expenses: " + totalExpenses);
+	    System.out.println("");
+	}
+	
+
+	public List getSumOfBalanceOfMonth2() {
 		String balance;
 		double income = 0;
 		double expenses = 0;
 		double incomePlus = 0;
 		double expensesPlus = 0;
-		
+		String currentMonth = "";
+
 		Data data = new Data();
 		DataContainer myObject = data.gson.fromJson(data.jsonString, DataContainer.class);
 		List<HandleData> dataList = myObject.getData();
 		for (HandleData item : dataList) {
+			 String month = item.getMonth();
+			 double value = item.getValue();
+		     boolean isExpense = item.getExpenses();
+			/*
+			 * if (item.getMonth().equals("Januar")) { }
+			 */
+			 
+			 if (!month.equals(currentMonth)) {
+				 if (!currentMonth.isEmpty()) {
+		                // Print the totals for the previous month
+		                System.out.println("Month: " + currentMonth);
+		                System.out.println("Income: " + income);
+		                System.out.println("Expenses: " + expenses);
+		            }
+				 
+				 currentMonth = month;
+		         income = 0;
+		         expenses = 0;
+		         
+		         if (isExpense) {
+		             expenses += value;
+		         } else {
+		             income += value;
+		         }
+			 }
+			 
+			 System.out.println("Month: " + currentMonth);
+			 System.out.println("Income: " + income);
+			 System.out.println("Expenses: " + expenses);
+
+			 /*
 			if (item.getExpenses() == true) {
 				System.out.println("TRUUEEEE");
 				System.out.println(item.getExpenses());
 				System.out.println(item.getValue());
 				incomePlus = item.getValue();
-				
+
 				income = income + incomePlus;
 			}
-			
+
 			if (item.getExpenses() == false) {
 				System.out.println("-------------FalsiFalsiFALSI");
 				System.out.println(item.getExpenses());
 				System.out.println(item.getValue());
 				expensesPlus = item.getValue();
-				
+
 				expenses = expenses + expensesPlus;
 			}
+			// System.out.println(item.getMonth());
+			 * 
+			 */
 			
 
 		}
-		System.out.println("Income: " + income);
-		System.out.println("Expenses: " + expenses);
+		
+		//System.out.println("Income: " + income);
+		//System.out.println("Expenses: " + expenses);
 		return dataList;
 
 	}
-	
-	
+
 	public List getMaxOfYear() {
 		Data data = new Data();
 		DataContainer myObject = data.gson.fromJson(data.jsonString, DataContainer.class);
 		List<HandleData> dataList = myObject.getData();
 		double value = 0;
 		double maxValue = 0;
-		
+
 		for (HandleData item : dataList) {
 			value = item.getValue();
-			
+
 			if (value > maxValue) {
 				maxValue = value;
 			}
 		}
-		
+
 		System.out.println("This is the max value: " + maxValue);
-		
+
 		return dataList;
 	}
-	
+
 	/*
-	public List getMinOfYear() {
-		Data data = new Data();
-		DataContainer myObject = data.gson.fromJson(data.jsonString, DataContainer.class);
-		List<HandleData> dataList = myObject.getData();
-		//double value;
-		double minValue = 500;
-		
-		Collections.max();
-		
-		for (HandleData item : dataList) {
-			double value = item.getValue();
-			//double minValue = item.getValue();
-			
-			if (value < minValue) {
-				minValue = value;
-			}
-		}
-		
-		System.out.println("This is the min value: " + minValue);
-		
-		return dataList;
-	}
-	*/
-	
+	 * public List getMinOfYear() { Data data = new Data(); DataContainer myObject =
+	 * data.gson.fromJson(data.jsonString, DataContainer.class); List<HandleData>
+	 * dataList = myObject.getData(); //double value; double minValue = 500;
+	 * 
+	 * Collections.max();
+	 * 
+	 * for (HandleData item : dataList) { double value = item.getValue(); //double
+	 * minValue = item.getValue();
+	 * 
+	 * if (value < minValue) { minValue = value; } }
+	 * 
+	 * System.out.println("This is the min value: " + minValue);
+	 * 
+	 * return dataList; }
+	 */
+
 }
