a480994b0b342d19406996725eb03a08f89f22b9
lauchbertia <111848605+lauchbertia@users.noreply.github.com>
2023-10-24 09:46:19 +0200

    minmax of months

--- src.main.java/budgetPlanning/HandleData.java
+++ src.main.java/budgetPlanning/HandleData.java
@@ -1,5 +1,6 @@
 package budgetPlanning;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -68,52 +69,49 @@ public class HandleData {
 		}
 		return dataList;
 	}
-	
+
 	public void getSumOfBalanceOfMonth() {
-	    Data data = new Data();
-	    DataContainer myObject = data.gson.fromJson(data.jsonString, DataContainer.class);
-	    List<HandleData> dataList = myObject.getData();
+		Data data = new Data();
+		DataContainer myObject = data.gson.fromJson(data.jsonString, DataContainer.class);
+		List<HandleData> dataList = myObject.getData();
 
-	    String currentMonth = "";
-	    double totalIncome = 0;
-	    double totalExpenses = 0;
+		String currentMonth = "";
+		double totalIncome = 0;
+		double totalExpenses = 0;
 
-	    for (HandleData item : dataList) {
-	        String month = item.getMonth();
-	        double value = item.getValue();
-	        boolean isExpense = item.getExpenses();
+		for (HandleData item : dataList) {
+			String month = item.getMonth();
+			double value = item.getValue();
+			boolean isExpense = item.getExpenses();
 
-	        if (!month.equals(currentMonth)) {
-	            if (!currentMonth.isEmpty()) {
-	                // Print the totals for the previous month
-	                System.out.println("Month: " + currentMonth);
-	                System.out.println("Income: " + totalIncome);
-	                System.out.println("Expenses: " + totalExpenses);
-	            }
-	            
-	            // Reset totals for the new month
-	            currentMonth = month;
-	            totalIncome = 0;
-	            totalExpenses = 0;
-	        }
+			if (!month.equals(currentMonth)) {
+				if (!currentMonth.isEmpty()) {
+					// Print the totals for the previous month
+					System.out.println("Month: " + currentMonth);
+					System.out.println("Income: " + totalIncome);
+					System.out.println("Expenses: " + totalExpenses);
+				}
 
-	        if (isExpense) {
-	            totalExpenses += value;
-	        } else {
-	            totalIncome += value;
-	        }
-	    }
+				// Reset totals for the new month
+				currentMonth = month;
+				totalIncome = 0;
+				totalExpenses = 0;
+			}
 
-	    // Print the totals for the last month
-	    System.out.println("");
-	    System.out.println("Month: " + currentMonth);
-	    System.out.println("Income: " + totalIncome);
-	    System.out.println("Expenses: " + totalExpenses);
-	    System.out.println("");
+			if (isExpense) {
+				totalExpenses += value;
+			} else {
+				totalIncome += value;
+			}
+		}
+
+		// Print the totals for the last month
+		System.out.println("");
+		System.out.println("Month: " + currentMonth);
+		System.out.println("Income: " + totalIncome);
+		System.out.println("Expenses: " + totalExpenses);
+		System.out.println("");
 	}
-	
-
-	
 
 	public List getMaxOfYear() {
 		Data data = new Data();
@@ -152,4 +150,67 @@ public class HandleData {
 	 * return dataList; }
 	 */
 
+	public void displayMaxMinForMonths() {
+	    Data data = new Data();
+	    DataContainer myObject = data.gson.fromJson(data.jsonString, DataContainer.class);
+	    List<HandleData> dataList = myObject.getData();
+
+	    String[] months = {"Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+
+	    // Initialize arrays to store max and min values for each month
+	    double[] maxIncomeByMonth = new double[12];
+	    double[] minIncomeByMonth = new double[12];
+	    double[] maxExpenseByMonth = new double[12];
+	    double[] minExpenseByMonth = new double[12];
+	    
+	 // Initialize minIncomeByMonth and minExpenseByMonth with high initial values
+	    Arrays.fill(minIncomeByMonth, Double.MAX_VALUE);
+	    Arrays.fill(minExpenseByMonth, Double.MAX_VALUE);
+
+	    for (HandleData item : dataList) {
+	        String month = item.getMonth();
+	        int monthIndex = Arrays.asList(months).indexOf(month);
+	        boolean isExpense = item.getExpenses();
+
+	        double value = item.getValue();
+
+	        // Update max and min values for the specific month
+	        if (!isExpense) {
+	            if (value > maxIncomeByMonth[monthIndex]) {
+	                maxIncomeByMonth[monthIndex] = value;
+	            }
+	            if (value < minIncomeByMonth[monthIndex]) {
+	                minIncomeByMonth[monthIndex] = value;
+	                
+	            }
+	        } else {
+	            if (value > maxExpenseByMonth[monthIndex]) {
+	                maxExpenseByMonth[monthIndex] = value;
+	            }
+	            if (value < minExpenseByMonth[monthIndex]) {
+	                minExpenseByMonth[monthIndex] = value;
+	            }
+	        }
+	    }
+
+	    // Output the results for each month
+	    for (int i = 0; i < 12; i++) {
+	        String month = months[i];
+	        System.out.println("----------------------------------------");
+	        System.out.println(month);
+	        System.out.println("----------------------------------------");
+	        System.out.println("\tEinkommen");
+	        System.out.println("\t \tmaximum: " + maxIncomeByMonth[i]);
+	        System.out.println("\t \tminimum: " + minIncomeByMonth[i]);
+        	System.out.println("\tAusgaben");
+	        System.out.println("\t \tmaximum: " + maxExpenseByMonth[i]);
+	        System.out.println("\t \tminimum: " + minExpenseByMonth[i]);
+	        System.out.println("");
+	        
+	    
+	
+		}
+	    
+	    
+	}
 }
