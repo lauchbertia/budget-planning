624c8a04d65518b44ac9884111548111ac86fd2d
lauchbertia <111848605+lauchbertia@users.noreply.github.com>
2023-10-29 17:29:52 +0100

    imperativ javadoc

--- src.main.java/budgetPlanning/Data.java
+++ src.main.java/budgetPlanning/Data.java
@@ -2,6 +2,13 @@ package budgetPlanning;
 
 import com.google.gson.Gson;
 
+/**
+ * GSON Object for JSON data
+ * JSON data as an String
+ * @see DataContainer#getData()
+ * 
+ */
+
 public class Data {
 	Gson gson = new Gson();
 	String jsonString = "{\n"
